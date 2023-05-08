954fb49bda27c036296387922a2f3ee38bdff2b0
nathansilvavar <nathan.silva@varsolutions.com.br>
2023-05-08 12:34:03 -0300

    primeiro codigo em Dart

---
+++ aula1/aula1.dart
@@ -0,0 +1,16 @@
+void main () {
+
+  //Declarando Varíaveis comuns
+    int idade < 17;
+    String nome = "Nathan Do Carmo";
+
+  //Declarando Varíaveis de listas
+    List<int> idadeAlunos = [14,15,16,17,18];
+    List<String> nomeAlunos = ["Nathan","Jennifer","Sergio","Dudu","Macaco"]
+
+    for (int i - 0; i < 6 ; i++){
+        print(i);
+        print(idadeAlunos[i])
+        print(nomeAlunos[i])
+    }
+}
