cf999704b5504013003828d29a0b8dc2c8a21241
Nathan Do Carmo <nathan.silva26@gmail.com>
2023-09-04 12:31:39 -0300

    update

---
+++ aula1lilia/funcoes.dart
@@ -0,0 +1,13 @@
+void main (){
+  print(calcular(2, 3));
+}
+
+double calcular(double num1, double num2){
+  print("Calcular");
+
+  return num1 * num2;
+}
+
+void Escrever(){
+  print("Função de escrever");
+}
