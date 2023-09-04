feed6d0429c95c7c059bf7336194cc42631ab2a2
Nathan Do Carmo <nathan.silva26@gmail.com>
2023-09-04 12:11:13 -0300

    exercicios de dart

---
+++ aula1lilia/ex1.dart
@@ -0,0 +1,66 @@
+void main() {
+  // var variavel = 'arrox';
+
+  // String nome = "nome";
+  // int idade = 17;
+  // double altura = 1.70;
+  // bool boliviano = true;
+
+  // print(variavel);
+  // print("\n Nome: $nome");
+  // print("\n Idade: $idade");
+  // print("\n Altura: $altura");
+  // print("\n Boliviano: $boliviano \n");
+
+  var array = ["Nathan", 17, "Dev", "Barueri"];
+
+  // print(" ");
+  // print(array.length);
+  // print(array);
+
+  // array.add("incremento");
+
+  // print(" ");
+  // print(array.length);
+  // print(array);
+
+  //--------------------------Condicionais-------------------------------------------
+
+  // bool x = true;
+
+  // if (x) {
+  //   print("Verdadeiro");
+  // } else {
+  //   print("Falso");
+  // }
+
+  //--------------------------Repetição-------------------------------------------
+
+  // int i = 0;
+  // while (i < array.length) {
+  //   print(array[i]);
+  //   i++;
+  // }
+
+  // for(int c = 0; c < array.length; c++){
+  //   print(array[c]);
+  // }
+
+  // for(var item in array){
+  //   print(item);
+  // }
+
+  switch ("caso 3") {
+    case "Caso 1":
+      print("Caso 1");
+      break;
+    case "Caso 2":
+      print("Caso 2");
+      break;
+    case "Caso 3":
+      print("Caso 3");
+      break;
+    default:
+      print("Nenhum caso selecionado");
+  }
+}
